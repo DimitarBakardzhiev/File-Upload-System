59d4e19435ea49a84b342957b5877d791c2e6db4
Rossoner40 <dimitar.bakardzhiev94@gmail.com>
2014-12-11 14:33:26 +0200

    Custom validator for username length added.

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,13 @@
 module.exports = function (mongoose) {
+    var MIN = 6;
+    var MAX = 20;
+
+    function length(value) {
+        return value.length >= MIN && value.length <= MAX;
+    }
+
     var userSchema = mongoose.Schema({
-        username: { type: String, required: true, unique: true },
+        username: { type: String, validate: length, required: true, unique: true },
         passwordHash: String,
         salt: String,
         points: Number
